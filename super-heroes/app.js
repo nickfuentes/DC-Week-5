65a0cb3b0c574363ea202262112f125d9a2dcf60
Full Name <nickfuentes24@gmail.com>
2019-07-22 11:36:14 -0500

    Initial Commit, got url and requesting a GET.

---
+++ super-heroes/app.js
@@ -0,0 +1,10 @@
+// grabs the div from the DOM
+let movieListDiv = document.getElementById("moveListDiv")
+// url with api key
+let batmanURL = http://www.omdbapi.com/?s=batman&apikey=c8b2e508
+
+    let req = new XMLHttpRequest()
+req.open("GET", batmanURL)
+req.addEventListener("load", () => {
+
+})
