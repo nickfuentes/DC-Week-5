c59f29d07f4badfc49092a99de03adad6de66d94
Full Name <nickfuentes24@gmail.com>
2019-07-22 17:56:25 -0500

    Created a onclick on image to show more detailed information on the movie selected.

--- super-heroes/app.js
+++ super-heroes/app.js
@@ -1,15 +1,17 @@
 // grabs the div from the DOM
 let movieListDiv = document.getElementById("movieListDiv")
+// grabs the a link form the DOM
+let detailedLink = document.getElementById("detailedLink")
+// grabs the detailed div 
+let detailedMovieDiv = document.getElementById("detailedMovieDiv")
+
 // APIKEY
 let apiKey = "c8b2e508"
-// DETAILED imbdID
-let imdbID = "tt4853102"
+
 
 // url with api key to get all batman movies
 let batmanURL = `http://www.omdbapi.com/?s=batman&apikey=${apiKey}`
 
-// url with api key to get detailed batman movies
-let detailedBatmanURL = `http://www.omdbapi.com/?i=${imdbID}&apikey=${apiKey}`
 
 // Reuqest New GET POST
 let reqBatman = new XMLHttpRequest()
@@ -21,17 +23,40 @@ reqBatman.addEventListener("load", () => {
 
 
     let movieItems = movies.Search.map(movie => {
-        return `<div>                                      
-                    <h2><a class="detailedLink" href= "${detailedBatmanURL}" >${movie.Title}</a></h2>
-                    <img src='${movie.Poster}' />
+        return `<div>   
+                    <img onclick="detailedFeature('${movie.imdbID}')" src='${movie.Poster}'/> 
+                    <h2>${movie.Title}</h2>
                 </div>`
-
     })
-    movieListDiv.innerHTML = movieItems.join('')
+    movieListDiv.innerHTML = movieItems.join("")
 })
 
 // make the actual request...
 reqBatman.send()
 
+function detailedFeature(imdbID) {
 
+    // url with api key to get detailed batman movies
+    let detailedBatmanURL = `http://www.omdbapi.com/?i=${imdbID}&apikey=${apiKey}`
 
+    // call a new GET request
+    let reqDetailedBatman = new XMLHttpRequest()
+    reqDetailedBatman.open("GET", detailedBatmanURL)
+
+    reqDetailedBatman.addEventListener("load", function () {
+        let moviesFeature = JSON.parse(event.currentTarget.responseText)
+
+        let moviesFeatureTemplate =
+            `<div>  
+                 <h2>${ moviesFeature.Title}</h2> 
+                 <img src='${ moviesFeature.Poster}'/> 
+                 <p>${ moviesFeature.Year}</p>
+                 <p>${ moviesFeature.Rated}</p> 
+                 <p>${ moviesFeature.Released}</p> 
+                 <p>${ moviesFeature.Director}</p>  
+    </div>`
+
+        detailedMovieDiv.innerHTML = moviesFeatureTemplate
+    })
+    reqDetailedBatman.send()
+}
