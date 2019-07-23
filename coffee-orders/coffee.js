c6519ffd6d5042f80ab2b1582ced2374145a6847
Full Name <nickfuentes24@gmail.com>
2019-07-23 16:38:23 -0500

    Created seperate functions to display to DOM and search by email address.

--- coffee-orders/coffee.js
+++ coffee-orders/coffee.js
@@ -1,7 +1,18 @@
 let ordersListDiv = document.getElementById("ordersListDiv")
 
+let newOrderButton = document.getElementById("newOrderButton")
+let searchOrderButton = document.getElementById("searchOrderButton")
+let deleteOrderButton = document.getElementById("deleteOrderButton")
 
-function getAllCoffeeOders() {
+let emailInputTextBox = document.getElementById("emailInputTextBox")
+let orderInputTextBox = document.getElementById("orderInputTextBox")
+let searchEmailInputTextBox = document.getElementById("searchEmailInputTextBox")
+
+let deleteEmailInputTextBox = document.getElementById("deleteEmailInputTextBox")
+let deleteEmailText = deleteEmailInputTextBox.value
+
+// Displays all the coffee orders onto the page
+function grabAllCoffeeOrders(getThemOrders) {
 
     let allCoffeOrders = "http://dc-coffeerun.herokuapp.com/api/coffeeorders/"
 
@@ -9,19 +20,65 @@ function getAllCoffeeOders() {
         .then(response => {
             return response.json() // response.json() returns a promise 
         }).then(json => {
-            return json
+            return Object.values(json)
         }).then((orders) => {
-            return Object.values(orders)
-        }).then((orders) => {
-            let allOrders = orders.map(order => {
-                return `<div class="orders">
-                <p>Email: ${order.emailAddress}</p>
-                <p>Coffee: ${order.coffee}</p>
-                </div>`
-            })
-            ordersListDiv.innerHTML = allOrders.join('')
+            getThemOrders(orders)
         })
 }
+grabAllCoffeeOrders(getThemOrders)
 
-getAllCoffeeOders()
+// pulls all orders out from grab CoffeeOrders function
+function getThemOrders(orders) {
+    displayAllOrders(orders)
+}
 
+// displays all cofffe orders
+function displayAllOrders(orders) {
+    let allOrders = orders.map(order => {
+        return `<div class="orders">
+        <p>Email: ${order.emailAddress}</p>
+        <p>Coffee: ${order.coffee}</p>
+        </div>`
+    })
+    ordersListDiv.innerHTML = allOrders.join("")
+}
+
+newOrderButton.addEventListener("click", postOrder)
+
+// post a new order to the api
+function postOrder() {
+
+    let emailTextCreateOrder = emailInputTextBox.value
+    let orderTextCreateOrder = orderInputTextBox.value
+
+    fetch("http://dc-coffeerun.herokuapp.com/api/coffeeorders/", {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ emailAddress: emailTextCreateOrder, coffee: orderTextCreateOrder })
+    })
+}
+
+
+searchOrderButton.addEventListener("click", getOrderByEmail)
+
+// gets order by email
+function getOrderByEmail() {
+
+    let searchEmailText = searchEmailInputTextBox.value
+
+    let coffeOrder = `http://dc-coffeerun.herokuapp.com/api/coffeeorders/${searchEmailText}`
+
+    fetch(coffeOrder)
+        .then(response => {
+            return response.json() // response.json() returns a promise 
+        }).then(order => {
+            let selectedOrder =
+                `<div class="orders">
+                       <p>Email: ${order.emailAddress}</p>
+                       <p>Coffee: ${order.coffee}</p>
+                     </div>`
+
+            ordersListDiv.innerHTML = selectedOrder
+        })
+
+}
